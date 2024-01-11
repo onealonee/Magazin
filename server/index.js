b7a0eae3501855dad45bd0b3572c1e120b25b9c5
onealonee <kazimiov.sk.xx@mail.ru>
2024-01-11 16:04:10 +0300

    1

--- server/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-const express = require('express')
-
-const PORT = process.env.PORT || 5000
-
-const app = express()
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
